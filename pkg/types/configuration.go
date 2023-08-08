18ad5bc077e8d2edafe2e2ed5f998ac5697ed8c8
Jake Correnti <jakecorrenti+github@proton.me>
2023-08-08 14:48:01 -0400

    `AddSocketsFromCmdline` functionality
    Adds the method `AddSocketsFromCmdline` to the `types.Configuration`
    type. Takes the validation code out of the main function and moves it to
    a method of the `type.Configuration` type.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- pkg/types/configuration.go
+++ pkg/types/configuration.go
@@ -2,7 +2,19 @@ package types
 
 import (
 	"net"
+	"net/url"
+	"os"
 	"regexp"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	ListenVpnkit = "listen-vpnkit"
+	ListenQemu   = "listen-qemu"
+	ListenBess   = "liten-bess"
+	ListenStdio  = "listen-stdio"
+	ListenVfkit  = "listen-vfkit"
 )
 
 type Configuration struct {
@@ -50,6 +62,9 @@ type Configuration struct {
 
 	// Protocol to be used. Only for /connect mux
 	Protocol Protocol
+
+	// Sockets is a map of the sockets provided by the user with format socket-type:socket
+	Sockets map[string]string
 }
 
 type Protocol string
@@ -87,5 +102,102 @@ func (i *ArrayFlags) String() string {
 
 func (i *ArrayFlags) Set(value string) error {
 	*i = append(*i, value)
+	return nil
+}
+
+// validateQemuSocket makes sure the qemu socket provided has proper syntax
+func validateQemuSocket(socket string) error {
+	if len(socket) > 0 {
+		uri, err := url.Parse(socket)
+		if err != nil || uri == nil {
+			return errors.Wrapf(err, "invalid value for listen-qemu")
+		}
+		if _, err := os.Stat(uri.Path); err == nil && uri.Scheme == "unix" {
+			return errors.Errorf("%q already exists", uri.Path)
+		}
+	}
+	return nil
+}
+
+// validateBessSocket makes sure the bess socket provided has proper syntax
+func validateBessSocket(socket string) error {
+	if len(socket) > 0 {
+		uri, err := url.Parse(socket)
+		if err != nil || uri == nil {
+			return errors.Wrapf(err, "invalid value for listen-bess")
+		}
+		if uri.Scheme != "unixpacket" {
+			return errors.New("listen-bess must be unixpacket:// address")
+		}
+		if _, err := os.Stat(uri.Path); err == nil {
+			return errors.Errorf("%q already exists", uri.Path)
+		}
+	}
+	return nil
+}
+
+// validateVfkitSocket makes sure the vfki socket provided has proper syntax
+func validateVfkitSocket(socket string) error {
+	if len(socket) > 0 {
+		uri, err := url.Parse(socket)
+		if err != nil || uri == nil {
+			return errors.Wrapf(err, "invalid value for listen-vfkit")
+		}
+		if uri.Scheme != "unixgram" {
+			return errors.New("listen-vfkit must be unixgram:// address")
+		}
+		if _, err := os.Stat(uri.Path); err == nil {
+			return errors.Errorf("%q already exists", uri.Path)
+		}
+	}
+
+	return nil
+}
+
+// validateOnlyUsingSingleProtocol ensures that we are only using one protocol
+// at a time
+func validateOnlyUsingSingleProtocol(vpnkitSocket, qemuSocket, bessSocket string) error {
+	if vpnkitSocket != "" && qemuSocket != "" {
+		return errors.New("cannot use qemu and vpnkit protocol at the same time")
+	}
+	if vpnkitSocket != "" && bessSocket != "" {
+		return errors.New("cannot use bess and vpnkit protocol at the same time")
+	}
+	if qemuSocket != "" && bessSocket != "" {
+		return errors.New("cannot use qemu and bess protocol at the same time")
+	}
+	return nil
+}
+
+// AddSocketsFromCmdline adds the different sockets to the Configuration based
+// on the flags used in the commandline by the user
+func (c *Configuration) AddSocketsFromCmdline(sockets map[string]string) error {
+	var vpnkitSocket, qemuSocket, bessSocket string
+
+	for flag, socket := range sockets {
+		var err error
+		switch flag {
+		case ListenQemu:
+			qemuSocket = socket
+			err = validateQemuSocket(socket)
+		case ListenBess:
+			bessSocket = socket
+			err = validateBessSocket(socket)
+		case ListenVfkit:
+			err = validateVfkitSocket(socket)
+		case ListenVpnkit:
+			vpnkitSocket = socket
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := validateOnlyUsingSingleProtocol(vpnkitSocket, qemuSocket, bessSocket); err != nil {
+		return err
+	}
+
+	c.Sockets = sockets
 	return nil
 }
